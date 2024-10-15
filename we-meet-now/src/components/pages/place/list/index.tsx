2b4086fca5da61151d2f4b2063a82f2717a4cd84
yeram-lim <yeramyeye@gmail.com>
2024-10-15 13:07:06 +0900

    😺[CHORE] 코드 정리(#11)

--- we-meet-now/src/components/pages/place/list/index.tsx
+++ we-meet-now/src/components/pages/place/list/index.tsx
@@ -12,10 +12,11 @@ declare global {
 
 const PlaceList = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const searchValue = searchParams.get("search");
+
   const [places, setPlaces] = useState<Place[]>([]);
-  var ps = new window.kakao.maps.services.Places();
+  const kakaoPlaces = new window.kakao.maps.services.Places();
 
   const search = (searchValue: string) => {
     navigate(`/place/list?search=${searchValue}`);
@@ -26,19 +27,20 @@ const PlaceList = () => {
       alert("키워드를 입력해주세요!");
       return false;
     }
-    ps.keywordSearch(searchValue, placesSearchCB);
-  };
-
-  const placesSearchCB = (data: any, status: any, pagination: any) => {
-    if (status === window.kakao.maps.services.Status.OK) {
-      setPlaces(data);
-    } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
-      alert("검색 결과가 존재하지 않습니다.");
-      return;
-    } else if (status === window.kakao.maps.services.Status.ERROR) {
-      alert("검색 결과 중 오류가 발생했습니다.");
-      return;
-    }
+    kakaoPlaces.keywordSearch(
+      searchValue,
+      (data: any, status: any, pagination: any) => {
+        if (status === window.kakao.maps.services.Status.OK) {
+          setPlaces(data);
+        } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
+          alert("검색 결과가 존재하지 않습니다.");
+          return;
+        } else if (status === window.kakao.maps.services.Status.ERROR) {
+          alert("검색 결과 중 오류가 발생했습니다.");
+          return;
+        }
+      }
+    );
   };
 
   useEffect(() => {
