1f2a94d8d394927b71e7a7ff9ac5749253d6087e
박태규 Park Tae Kyu <ptq124@naver.com>
2024-10-16 01:21:30 +0900

    ➕[ADD]: id root에 position relative 적용 #6

--- we-meet-now/src/styles/global-style.ts
+++ we-meet-now/src/styles/global-style.ts
@@ -1,8 +1,11 @@
-import { createGlobalStyle } from 'styled-components'
-import tw from 'twin.macro'
+import { createGlobalStyle } from "styled-components";
+import tw from "twin.macro";
 
 export const GlobalStyle = createGlobalStyle`
   body{
     ${tw`h-screen max-w-[390px] mx-auto border-2 border-rose-600`}
   }
-`
+  #root{
+    ${tw`relative`}
+  }
+`;
