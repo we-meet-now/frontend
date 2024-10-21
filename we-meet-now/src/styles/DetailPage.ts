4a5fc1a5042645c872af72e47b8270400b6dfc57
박태규 Park Tae Kyu <ptq124@naver.com>
2024-10-21 23:39:37 +0900

    🍭[STYLE] DetailPage 디자인 수정

--- we-meet-now/src/styles/DetailPage.ts
+++ we-meet-now/src/styles/DetailPage.ts
@@ -5,19 +5,22 @@ export const Container = styled.div`
   ${tw`absolute bottom-0 z-20 h-[200px] w-full bg-white rounded-t-[10%]`}
 
   .card {
-    ${tw`mt-7`}
+    ${tw`mx-auto mt-7 w-80`}
     .title {
-      ${tw`flex items-center justify-center gap-5`}
+      ${tw`flex items-center justify-start gap-2`}
 
       div:nth-child(1) {
         ${tw`text-[22px]`}
       }
-      div:last-child(2) {
-        ${tw`text-[10px]`}
+      div:nth-child(2) {
+        ${tw`text-[13px]`}
       }
     }
     .sub-information {
-      ${tw`flex flex-col justify-center items-center text-[13px] mt-4`}
+      ${tw`flex flex-col justify-start items-start text-[13px] mt-2 gap-1`}
+    }
+    svg {
+      ${tw`mt-5`}
     }
   }
 `;
